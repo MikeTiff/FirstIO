d8db097c22427340fc021ffc7ef332c435f7a66d
MikeTiff <mtiffany@mail.csuchico.edu>
2018-01-29 17:24:09 -0800

    Changes the greeting

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,10 @@
 
 int main() {
     std::string input;
+    std::cout << "Hello, my name is Michael" <<std::endl;
     std::cout << "What's your first name?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
+    // Author: Michael Tiffany
     return 0;
 }
